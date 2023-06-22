570eb633d7224386fc3273599fcb580ba4da58b9
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-06-22 22:06:24 +0200

    Move tests to root

---
+++ cli.py
@@ -0,0 +1,43 @@
+#!/Library/Frameworks/Python.framework/Versions/3.11/bin/python3 python
+
+from rentomatic.repository.memrepo import MemRepo
+from rentomatic.use_cases.room_list import room_list_use_case
+from pprint import pprint
+
+# rooms = [
+#     {
+#         "code": "f853578c-fc0f-4e65-81b8-566c5dffa35a",
+#         "size": 215,
+#         "price": 39,
+#         "longitude": -0.09998975,
+#         "latitude": 51.75436293,
+#     },
+#     {
+#         "code": "fe2c3195-aeff-487a-a08f-e0bdc0ec6e9a",
+#         "size": 405,
+#         "price": 66,
+#         "longitude": 0.18228006,
+#         "latitude": 51.74640997,
+#     },
+#     {
+#         "code": "913694c6-435a-4366-ba0d-da5334a611b2",
+#         "size": 56,
+#         "price": 60,
+#         "longitude": 0.27891577,
+#         "latitude": 51.45994069,
+#     },
+#     {
+#         "code": "eed76e77-55c1-41ce-985d-ca49bf6c0585",
+#         "size": 93,
+#         "price": 48,
+#         "longitude": 0.33894476,
+#         "latitude": 51.39916678,
+#     },
+# ]
+rooms = []
+
+repo = MemRepo(rooms)
+result = room_list_use_case(repo)
+
+pprint([room.to_dict() for room in result])
+
---
+++ tests/responses/test_responses.py
@@ -0,0 +1,4 @@
+from rentomatic.responses.responses import ResponseSuccess
+
+def test_response_success_is_true():
+  assert bool(ResponseSuccess()) is True
