720810ffdc260ad31ff1b5f74d47a649f3b3c316
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-06-25 20:45:01 +0200

    Error Management

--- tests/requests/test_room_list.py
+++ tests/requests/test_room_list.py
@@ -4,14 +4,31 @@ from rentomatic.requests.room_list import build_room_list_request
 
 def test_build_room_list_request_without_parameters():
     request = build_room_list_request()
+
     assert request.filters is None
     assert bool(request) is True
 
 
 def test_build_room_list_request_from_empty_dict():
     request = build_room_list_request({})
+
     assert request.filters == {}
     assert bool(request) is True
+
+def test_build_room_list_request_with_invalid_filters_parameter():
+    request = build_room_list_request(filters=5)
+
+    assert request.has_errors()
+    assert request.errors[0]["parameter"] == "filters"
+    assert bool(request) is False
+
+
+def test_build_room_list_request_with_incorrect_filter_keys():
+    request = build_room_list_request(filters={"a": 1})
+
+    assert request.has_errors()
+    assert request.errors[0]["parameter"] == "filters"
+    assert bool(request) is False
 
 
 @pytest.mark.parametrize("key", ["code__eq", "price__eq", "price__lt", "price__gt"])
